c60f3ac89dbd24d27d4e3e9f292ba8f18fbdb008
MrYotic <yoticcitoy@gmail.com>
2022-04-06 02:37:11 +0300

    f good

--- DidYouSayUkraine/Program.cs
+++ DidYouSayUkraine/Program.cs
@@ -23,16 +23,16 @@ List<(Point, Vec3)> flagFind = new List<(Point, Vec3)>();
 
 for (int x = 0; x < ix; x++)
     for (int y = 0; y < iy; y++)
-        if (imageColors[x, y].IsColor(200, ColorType.B) || imageColors[x, y].IsColor(150, ColorType.R, ColorType.G))
+        if (imageColors[x, y].IsColor(200, ColorType.B) || imageColors[x, y].IsColor(200, ColorType.R, ColorType.G))
             flagFind.Add((new(x, y), imageColors[x, y]));
 
 List<int> coordsX = flagFind.Select(z => z.Item1.X).Distinct().ToList();
 List<(int x, List<(Point, Vec3)> yellows, List<(Point, Vec3)> blues, int startY, int yellowCount, int blueCount, int allCount, int perColor, int extraLastColor)> rawPixels = new List<(int x, List<(Point, Vec3)> yellows, List<(Point, Vec3)> blues, int startY, int yellowCount, int blueCount, int allCount, int perColor, int extraLastColor)>();
 foreach(int x in coordsX)
 {
-    List<(Point, Vec3)> yellows = flagFind.FindAll(z => z.Item1.X == x && z.Item2.IsColor(150, ColorType.R, ColorType.G));
+    List<(Point, Vec3)> yellows = flagFind.FindAll(z => z.Item1.X == x && z.Item2.IsColor(200, ColorType.R, ColorType.G));
     List<(Point, Vec3)> blues = flagFind.FindAll(z => z.Item1.X == x && z.Item2.IsColor(200, ColorType.B));
-    int startY = flagFind.FindAll(z => z.Item1.X == x && (z.Item2.IsColor(150, ColorType.R, ColorType.G) || z.Item2.IsColor(200, ColorType.B))).OrderBy(z => z.Item1.Y).First().Item1.Y;
+    int startY = flagFind.FindAll(z => z.Item1.X == x && (z.Item2.IsColor(200, ColorType.R, ColorType.G) || z.Item2.IsColor(200, ColorType.B))).OrderBy(z => z.Item1.Y).First().Item1.Y;
     int yellowCount = yellows.Count;
     int blueCount = blues.Count;
     int allCount = yellowCount + blueCount;
@@ -42,8 +42,9 @@ foreach(int x in coordsX)
 }
 List<(int, int)> rawOneOfThreeList = rawPixels.Where(z => z.yellowCount > 0 && z.blueCount > 0).Select(z => (z.yellowCount, z.blueCount)).ToList();
 int maxOneOfThree = rawOneOfThreeList.GroupBy(z => z.Item2).ToDictionary(z => z.Key, g => g.Count()).ToList().Select(z => z.Value).Max();
+int minY = 0;
 foreach((int x, List<(Point, Vec3)> yellows, List<(Point, Vec3)> blues, int startY, int yellowCount, int blueCount, int allCount, int perColor, int extraLastColor) pixel in rawPixels)
-{
+{    
     if((pixel.blueCount + pixel.yellowCount) / 3 > maxOneOfThree)
     {
         for (int w = 0; w < pixel.perColor; w++)
@@ -54,30 +55,23 @@ foreach((int x, List<(Point, Vec3)> yellows, List<(Point, Vec3)> blues, int star
             image.SetPixel(pixel.x, pixel.startY + r, Color.Red);
         for (int e = 0; e < pixel.extraLastColor + 1; e++)
             image.SetPixel(pixel.x, pixel.startY + pixel.perColor * 3 + e, Color.Red);
+        if (pixel.startY < minY) minY = pixel.startY;
     }
     else
     {
-
-        //int count = rawPixels.Find(z => z.x == pixel.x).yellowCount + rawPixels.Find(z => z.x == pixel.x).blueCount;
-        for (int w = 0; w < pixel.perColor; w++)
-            if (flagFind.FindIndex(z => z.Item1.X == pixel.x && z.Item1.Y == pixel.startY + w) != -1)                
-                image.SetPixel(pixel.x, pixel.startY + w, Color.White);
-        for (int b = pixel.perColor; b < pixel.perColor * 2; b++)
-            if (flagFind.FindIndex(z => z.Item1.X == pixel.x && z.Item1.Y == pixel.startY + b) != -1)
-                image.SetPixel(pixel.x, pixel.startY + b, Color.Blue);
-        for (int r = pixel.perColor * 2; r < pixel.perColor * 3; r++)
-            if (flagFind.FindIndex(z => z.Item1.X == pixel.x && z.Item1.Y == pixel.startY + r) != -1)
-                image.SetPixel(pixel.x, pixel.startY + r, Color.Red);
-        /*
-        for (int w = 0; w < (count - maxOneOfThree < 0 ? maxOneOfThree : count - maxOneOfThree); w++)
-            image.SetPixel(pixel.x, pixel.startY + w, Color.White);
-        for (int b = (count - maxOneOfThree < 0 ? maxOneOfThree : count - maxOneOfThree); b < (count - maxOneOfThree * 2 < 0 ? maxOneOfThree : count - maxOneOfThree * 2); b++)
-            image.SetPixel(pixel.x, pixel.startY + b, Color.Blue);
-        for (int r = count - maxOneOfThree * 2; r < (count - maxOneOfThree * 3 < 0 ? maxOneOfThree : count - maxOneOfThree * 3); r++)
-            image.SetPixel(pixel.x, pixel.startY + r, Color.Red);/*
-        for (int e = pixel.perColor * 3; e < pixel.extraLastColor; e++)
-            image.SetPixel(pixel.x, pixel.startY + e, Color.Red);
-        */
+        rawPixels.FindAll(z => z.x == pixel.x).ForEach(z => 
+        {
+            List<int> yCoords = z.yellows.Concat(z.blues).Select(z => z.Item1.Y).ToList();
+            yCoords.ForEach(x =>
+            {
+                if(x - pixel.startY <= maxOneOfThree)
+                    image.SetPixel(pixel.x, x, Color.White);
+                else if(x - pixel.startY <= maxOneOfThree * 2)
+                    image.SetPixel(pixel.x, x, Color.Blue);
+                else
+                    image.SetPixel(pixel.x, x, Color.Red);
+            });
+        });
     }
     WriteLine(pixel.yellowCount + " - " + pixel.blueCount);
 }
